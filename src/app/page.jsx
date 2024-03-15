5a9a9942ce71687c9d04d0c001e7e861477fc95b
AxelMendoza05 <82965069+AxelMendoza05@users.noreply.github.com>
2024-03-14 19:03:07 -0600

    end

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,36 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import "../assets/Styles/Pages.css";
 import Modal from "../components/Modal";
 import Login from "../components/Login";
 import Button from "../components/Button";
+
 export default function Home() {
   const router = useRouter();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   const handleLoginData = () => {
     console.log(username, password);
-    if (username === "onyx" && password === "onyx") {
+    if (username === "admin" && password === "admin") {
       router.push("/NewBook");
     }
   };
 
+  if (!isClient) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="fullScreenCenter">
       <div className="card shadow">
-        <h1></h1>
+        <h1>Logo</h1>
         <div>
           <Modal
             title="Login"
